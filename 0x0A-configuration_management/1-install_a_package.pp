6649e63c241dcb7239de126acc2e8b61a555f6bd
Alexander olamide Kolurejo <Kolurejoalexanderola@gmail.com>
2024-03-23 16:35:48 +0100

    done:

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
-#we are trying to install a package named flask
+# we are trying to install a package named flask
 
-package{'flask'
-    ensure =>'2.1.0',
-    provider =>'pip3'
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
