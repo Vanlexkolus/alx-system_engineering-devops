19efb6bcabb655bd45873a5b14352b0d15880f87
Alexander olamide Kolurejo <Kolurejoalexanderola@gmail.com>
2024-03-25 13:52:01 +0100

    done

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#usr/bin/pup
+#	using puppet to configure a server
+
+line { 'Turn off passwd auth':
+  path  => '/etc/ssh/sshd_config',
+  line  => 'PasswordAuthentication no',
+  match => '^#?PasswordAuthentication',
+}
+
+line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^#?IdentityFile',
+}
+
